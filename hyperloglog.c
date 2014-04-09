50d91fed71380d6b2578ce94d06937e060899b71
Lukas Havrlant <lukas@havrlant.cz>
2014-04-09 09:44:13 +0200

    refactoring

--- hyperloglog.c
+++ hyperloglog.c
@@ -6,7 +6,6 @@
 #if defined(__APPLE__)
 #  define COMMON_DIGEST_FOR_OPENSSL
 #  include <CommonCrypto/CommonDigest.h>
-#  define SHA1 CC_SHA1
 #else
 #  include <openssl/md5.h>
 #endif
@@ -20,103 +19,106 @@
 
 typedef unsigned char byte;
 typedef unsigned int uint;
-typedef char*(GENERATOR)(FILE*);
-const uint HASH_LENGTH = 32;
-const int BITS_IN_BYTES = 8;
-// const byte bitflags[] = {128, 64, 32, 16, 8, 4, 2, 1};
-const byte bitflags[] = {1, 2, 4, 8, 16, 32, 64, 128};
+const int BITS_IN_BYTE = 8;
+
+typedef struct {
+  uint loglog;
+  uint hyperloglog;
+} cardinalities;
 
 unsigned char *str2md5(const char *str, int length) {
-    int n;
     MD5_CTX c;
     unsigned char *digest = (unsigned char *)malloc(sizeof(unsigned char) * 16);
-
+    const int blocklength = 512;
     MD5_Init(&c);
 
     while (length > 0) {
-        if (length > 512) {
-            MD5_Update(&c, str, 512);
+        if (length > blocklength) {
+            MD5_Update(&c, str, blocklength);
         } else {
             MD5_Update(&c, str, length);
         }
-        length -= 512;
-        str += 512;
+        length -= blocklength;
+        str += blocklength;
     }
 
     MD5_Final(digest, &c);
-
     return digest;
 }
 
-unsigned int max(unsigned int a, unsigned int b) {
+uint max(uint a, uint b) {
   return a > b ? a : b;
 }
 
-uint rho(const unsigned char * buffer, uint nbits, uint bitfrom) {
+/*
+Vrati pozici nejlevejsiho bitu, ktery je roven 1.
+Zacina hledat od index bitfrom. Indexuje se od 1.
+rho(1001000010, 10, 4) = 6
+            ^
+*/
+uint rho(const byte *digest, uint bitlength, uint bitfrom) {
     uint b = 0;
     uint byteIdx = 0;
     uint bitIdx = 0;
-    for (b = bitfrom; b < nbits; b++) {
-        byteIdx = b / 8;
-        bitIdx  = b % 8;
-        if ((buffer[byteIdx] & (0x1 << bitIdx)) != 0)
+    for (b = bitfrom; b < bitlength; b++) {
+        byteIdx = b / BITS_IN_BYTE;
+        bitIdx  = (BITS_IN_BYTE - 1) - b % BITS_IN_BYTE;
+        if ((digest[byteIdx] & (1 << bitIdx)) != 0)
             return (b - bitfrom + 1);
         
     }
     
-    return (nbits-bitfrom) + 1;
+    return (bitlength-bitfrom) + 1;
 
 }
 
-uint bIndex(byte *checksum, uint length) {
-  uint index = 0;
-  uint b;
-  uint byteIdx = 0;
-  uint bitIdx = 0;
-  for (b = 0; b < length; b++) {
-      byteIdx = b / 8;
-      bitIdx  = b % 8;
-      if ((checksum[byteIdx] & (1 << bitIdx)) != 0) {
-        index += (1 << (length - b - 1));
-      }
+/*
+Prevede prvnich bucketBitLength bitu na cislo
+bucketIndex(1001000010, 4) = 1001 = 9
+*/
+uint bucketIndex(byte *digest, uint bucketBitLength) {
+  uint index;
+  uint bytesHashLength = 4;
+  byte temparray[bytesHashLength];
+  for (int i = 0; i < bytesHashLength; i++) {
+    temparray[bytesHashLength - i - 1]  = digest[i];
   }
+  memcpy(&index, temparray, sizeof(uint));
+  index = index >> (32 - bucketBitLength);
   return index;
 }
 
-uint bucketIndex(byte *checksum, uint length) {
-  int index = 0;
-  int exponent;
-  int bit;
-  uint counter = 0;
-  for (uint i = 0; counter < length; i++) {
-    for (uint j = 0; j < BITS_IN_BYTES && counter < length; j++) {
-      exponent = length - ((i * BITS_IN_BYTES) + j) - 1;
-      bit = (checksum[i] & bitflags[j]) == bitflags[j];
-      if (bit) {
-        // printf("bucketIndex: %u, j: %u\n", exponent, j);
-      }
-      index += pow(2, exponent) * bit;
-      counter++;
-    }
+// Precte jeden radek ze souboru, odstrani \n z konce radku
+char *readline(FILE *fp, char *buffer, int max) {
+  char *res;
+  res = fgets(buffer, max, fp);
+  if (res) {
+    buffer[strlen(buffer) - 1] = 0;
+    return buffer;
+  } else {
+    return NULL;
   }
-  return index;
 }
 
-
-
-void computeMaxes(uint b, uint checksumLength, GENERATOR generator, byte *M) {
-  byte *checksum;
+// Spocita rho hodnoty pro vsechny vstupni retezce a ulozi je do spravnych buckets
+byte *computeMaxes(uint b, uint digestBitLength, FILE *fp, uint m) {
+  byte *digest;
   char *word;
-  uint j, jj, first1;
-  uint  counter = 0;
-  while ((word = generator(NULL)) != NULL) {
-    checksum = str2md5(word, strlen(word));
-    j = bIndex(checksum, b);
-    first1 = rho(checksum, checksumLength, b);
+  uint j, first1;
+  const uint bufferlength = 256; // maximalni delka slova na radku = 255
+  char buffer[bufferlength];
+  byte *M = (byte*) calloc(m, sizeof(byte));
+  while ((word = readline(fp, buffer, bufferlength)) != NULL) {
+    digest = str2md5(word, (int)strlen(word));
+    j = bucketIndex(digest, b);
+    first1 = rho(digest, digestBitLength, b);
     M[j] = max(M[j], first1);
-    free(checksum);
+    free(digest);
   }
+  return M;
 }
+
+
 
 /************************************************
 
@@ -126,22 +128,20 @@ void computeMaxes(uint b, uint checksumLength, GENERATOR generator, byte *M) {
 
 double computeLogLogCardinality(uint m, byte *M, double alpham) {
   double E = 0;
-  double sum = 0, average;
+  double sum = 0, arithmeticAverage;
   for (int j = 0; j < m; j++) {
     sum += M[j];
-    // printf("%u,", M[j]);
   }
-  average = sum / m;
-  E = alpham * m * pow(2, average);
-  // printf("\nsum %g, avg: %g, E: %g\n", sum, average, E);
+  arithmeticAverage = sum / m; 
+  E = alpham * m * pow(2, arithmeticAverage);
   return E;
 }
 
-double loglog(uint b, uint checksumLength, GENERATOR generator) {
+double loglog(uint b, uint digestBitLength, FILE *fp) {
   uint m = (uint) pow(2, b);
-  byte *M = (byte*) calloc(m, sizeof(byte));
-  computeMaxes(b, checksumLength, generator, M);
+  byte *M = computeMaxes(b, digestBitLength, fp, m);
   double cardinality = computeLogLogCardinality(m, M, 0.39701);
+  free(M);
   return cardinality;
 }
 
@@ -152,84 +152,63 @@ double loglog(uint b, uint checksumLength, GENERATOR generator) {
               HYPER LOG LOG
 
 *************************************************/
-double computeAlpha(unsigned int m) {
-  return (0.7213/(1+1.079/m));
+double computeHyperAlpha(unsigned int m) {
+  return 0.7213 / (1 + 1.079 / m);
 }
 
 double computeHyperCardinality(uint m, byte *M, double alpham) {
   double E = 0;
-  double sum = 0, average;
+  double sum = 0, harmonicAverage;
   for (int j = 0; j < m; j++) {
     sum += pow(2, -M[j]);
-    // printf("%u, ", M[j]);
   }
-  average = m / sum;
-  E = alpham * m * average;
+  harmonicAverage = m / sum; // harmonicky prumer
+  E = alpham * m * harmonicAverage;
   return E;
 }
 
+double applyCorrections(double E, uint m, byte *M) {
+  uint V = 0;
+  double Estar = E;
 
-double hyperloglog(uint b, uint checksumLength, GENERATOR generator) {
+  if (E <= ((5 / 2) * m)) {
+    for (uint i = 0; i < m; i++) {
+      if (M[i] == 0) {
+        V++;
+      }
+    }
+  }
+}
+
+double hyperloglog(uint b, uint digestBitLength, FILE *fp) {
   uint m = (uint) pow(2, b);
-  byte *M = (byte*) calloc(m, sizeof(byte));
-  computeMaxes(b, checksumLength, generator, M);
-  double cardinality = computeHyperCardinality(m, M, computeAlpha(m));
+  byte *M = computeMaxes(b, digestBitLength, fp, m);
+  double cardinality = computeHyperCardinality(m, M, computeHyperAlpha(m));
+  free(M);
   return cardinality;
 }
 
-
-// Input stream of words
-char *lines(FILE *fp) {
-  static FILE *inputfile;
-  static char *lineBuffer = (char*)malloc(128);
-  static size_t read;
-  static size_t len;
-
-  if (fp) {
-    inputfile = fp;
-    return NULL;
-  }
-
-  read = getline(&lineBuffer, &len, inputfile);
-  if (read != EOF) {
-    if (lineBuffer[read - 1] == '\n') {
-      lineBuffer[read - 1] = 0;
-    }
-    return lineBuffer;
-  } else {
-    fclose(inputfile);
-    return NULL;
-  }
-}
-
-
-
-
-void reverse(byte *arr, int length) {
-  byte temp;
-  for (int i = 0; i <= length / 2; i++) {
-    temp = arr[i];
-    arr[i] = arr[length - i - 1];
-    arr[length - i - 1] = temp;
-  }
+cardinalities bothlog(uint b, uint digestBitLength, FILE *fp) {
+  cardinalities card;
+  uint m = (uint) pow(2, b);
+  byte *M = computeMaxes(b, digestBitLength, fp, m);
+  card.loglog = (uint)computeLogLogCardinality(m, M, 0.39701);
+  card.hyperloglog = (uint)computeHyperCardinality(m, M, computeHyperAlpha(m));
+  free(M);
+  return card;
 }
 
 
 int main(int argc, char **argv) {
-  // int length = 4;
-  // byte checksum[] = {158,147,209,155,};
-  // printf("bucketIndex: %u\n", bucketIndex(checksum, length));
-  // printf("bIndex:%u\n", bIndex(checksum, length));
-    char *path = (char*)"1000000.txt";
+    char *path = (char*)"100.txt";
     if (argc > 1) {
       path = argv[1];
     }
     
     FILE *fp = fopen(path, "r");
-    lines(fp);
-    printf("hyperloglog: %g\n", hyperloglog(12, 32, lines));
-    fp = fopen(path, "r");
-    lines(fp);
-    printf("loglog:      %g\n", loglog(12, 32, lines));
+    // printf("hyperloglog: %u\n", (uint)hyperloglog(12, 32, lines));
+    // printf("loglog:      %u\n", (uint)loglog(12, 32, fp));
+    cardinalities card = bothlog(12, 32, fp);
+    printf("loglog:      %u\nhyperloglog: %u\n", card.loglog, card.hyperloglog);
     return 0;
 }
